e9eda52b39e0fa184e1a195ddd6feacc70181a1a
yedimiii <yerim1240@naver.com>
2024-06-11 17:41:49 +0900

    Update - 날짜변경

--- src/App.vue
+++ src/App.vue
@@ -135,13 +135,13 @@ export default {
   data() {
     return {
       weekNames: [
+        '일요일',
         '월요일',
         '화요일',
         '수요일',
         '목요일',
         '금요일',
         '토요일',
-        '일요일',
       ],
       rootYear: 1904,
       rootDayOfWeekIndex: 4, // 2000년 1월 1일은 토요일
@@ -242,7 +242,7 @@ export default {
             sumOfDay += this.getEndOfDay(year, month);
             month++;
           } else if (targetMonth == month) {
-            return sumOfDay % 7;
+            return (sumOfDay + 1) % 7; // 일요일을 첫번째 열로 변경
           }
         }
       }
